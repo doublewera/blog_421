6fb55ad76d5e8a6bd357d5219cfa130e81777eb7
Odilbek Arziev <odarziev@mail.ru>
2024-12-16 19:32:28 +0300

    changes

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("", home, name="home"),
     path("post/<int:pk>/", post, name="post"),
+    path("create_post/", create_post, name="create_post"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
+from .forms import PostForm
 
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+    return render(request, "home.html", {"posts": posts})
+
 
 def post(request, pk):
     post_data = Post.objects.get(id=pk)
-    return render(request, 'post.html', {'post': post_data})
+    return render(request, "post.html", {"post": post_data})
+
+
+def create_post(request):
+    form = PostForm()
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect("app:home")
+
+    return render(request, "create_post.html", {"form": form})
